be558e1bfb8f13392f94bf7bc7e3b60c3116bd6e
imcvampire <quocanh96@gmail.com>
2016-12-02 15:15:37 +0700

    refact: .

--- vue-tag-list.js
+++ vue-tag-list.js
@@ -1,26 +1,27 @@
-;(function () {
-	var vueTagList = {}
+(function () {
+  var vueTagList = {}
 
-	vueTagList.install = function (Vue) {
-		Vue.filter('tagList', function (array, length, param, icon) {
-    if (!icon && icon !== '')
-      icon = '#'
+  vueTagList.install = function (Vue) {
+    Vue.filter('tagList', function (array, length, param, icon) {
+      if (!icon && icon !== '') icon = '#'
 
-		return array
-      .map(function (val) {
-		     return icon + (param ? val[param] : val) 
-		  })
-      .slice(0, length || array.length)
-      .join(', ')
-		})
-	}
+      return array
+        .map(function (val) {
+          return icon + (param ? val[param] : val)
+        })
+        .slice(0, length || array.length)
+        .join(', ')
+    })
+  }
 
-	if (typeof exports == "object") {
+  if (typeof exports == "object") {
     module.exports = vueTagList
   } else if (typeof define == "function" && define.amd) {
-    define([], function(){ return vueTagList })
+    define([], function () {
+      return vueTagList
+    })
   } else if (window.Vue) {
     window.VueTagList = vueTagList
     Vue.use(VueTagList)
   }
-})()
+})();
